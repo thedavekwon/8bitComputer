3ae87147334b5a0cc0b5689d5d963fbe677328b9
thedavekwon <thedavekwon@gmail.com>
2016-04-18 19:09:13 -0400

    changed to 8 bit

--- adder.v
+++ adder.v
@@ -1,8 +1,12 @@
-module single_adder();
-reg a,b;
-reg sum, carry;
+module eight_bit_adder();
+
+reg[7:0] a,b;
+reg[7:0] sum;
+reg carry;
 
 always @ (a or b)
 begin
 	{carry,sum} = a+b;
 end
+
+endmodule
