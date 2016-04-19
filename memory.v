946a6c62b1e37065517732551b6d27b45fb97219
thedavekwon <thedavekwon@gmail.com>
2016-04-18 21:17:02 -0400

    updated

--- memory.v
+++ memory.v
@@ -1,28 +1,52 @@
-module memory(address, data);
-   input [7:0] address;
-   output [7:0] data;
-
-   reg [7:0]    data;
-   reg [7:0] mainmem [0:256];
+module memory( 
+	output reg [7:0] data_out,
+	input [7:0] address,
+ 	input [7:0] data_in, 
+    input write_enable,
+    input clk);
+	
+   reg [7:0] mainmem [0:255];
 
    initial begin
       $readmemb("mem.bin", mainmem);
 
    end
-   always @ (address) begin
-      data = mainmem[address];
+   always @ (posedge clk) begin
+      data_out <= mainmem[address];
+   end
+
+   always @ (negedge clk & write_enable) begin
+   	  mainmem[address] <= data_in;
    end
 endmodule
 
 module test;
-   wire [7:0] stuff;
+   reg [7:0] stuff_in;
+   wire [7:0] stuff_out;
    reg [7:0] addr;
+   reg clk, write_enable;
 
-   memory mem(addr, stuff);
+   memory mem(stuff_out, addr, stuff_in, write_enable, clk);
 
-   initial begin
-      $display("Test: %d", stuff);
-      #5 addr = 8'b00000010;
-      #6 $display("Test: %d", stuff);
+
+  
+   initial begin 
+   	  clk = 0;
+   	  write_enable = 0;
+   	  addr = 8'b0000_0010;
+   	  $monitor("%d, address: %d",clk, addr);
+      #6 $display("Test: %d", stuff_out);
+      #8 $display("Test: %d", stuff_out);
+      #10 write_enable = 1;
+      #10 stuff_in = 8'b11111111;
+      #11$display("Test: %d", stuff_out);
+      #16 $display("Test: %d", stuff_out);
    end
+    
+   always 
+    #2 clk = !clk;
+
+   initial 
+      #100  $finish; 
+  
 endmodule
