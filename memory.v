ba33128155298aa011e3511e16801bc2441f9867
DanManN <dnahimov@gmail.com>
2016-04-18 21:43:50 -0400

    Actually works now

--- memory.v
+++ memory.v
@@ -1,52 +1,56 @@
-module memory( 
+module memory(
 	output reg [7:0] data_out,
-	input [7:0] address,
- 	input [7:0] data_in, 
-    input write_enable,
-    input clk);
-	
-   reg [7:0] mainmem [0:255];
+	input [7:0]      address,
+ 	input [7:0]      data_in,
+  input            write_enable,
+  input            clk);
+
+   reg [7:0]       mainmem [0:255];
+
+   integer         i;
 
    initial begin
       $readmemb("mem.bin", mainmem);
-
+      for (i=0; i < 5; i=i+1)
+        $display("%d:%b",i,mainmem[i]);
    end
+
    always @ (posedge clk) begin
-      data_out <= mainmem[address];
+      data_out = mainmem[address];
    end
 
-   always @ (negedge clk & write_enable) begin
-   	  mainmem[address] <= data_in;
+   always @ (negedge clk) begin
+      if (write_enable) begin
+   	     mainmem[address] = data_in;
+      end
    end
 endmodule
 
 module test;
    reg [7:0] stuff_in;
    wire [7:0] stuff_out;
-   reg [7:0] addr;
-   reg clk, write_enable;
+   reg [7:0]  addr;
+   reg        clk, write_enable;
 
    memory mem(stuff_out, addr, stuff_in, write_enable, clk);
 
-
-  
-   initial begin 
-   	  clk = 0;
-   	  write_enable = 0;
-   	  addr = 8'b0000_0010;
-   	  $monitor("%d, address: %d",clk, addr);
-      #6 $display("Test: %d", stuff_out);
-      #8 $display("Test: %d", stuff_out);
+   initial begin
+   	  #1 clk = 0;
+   	   write_enable = 0;
+   	   addr = 8'b0000_0010;
+   	  $monitor("%b, %d, %b, %b: time: %d",clk, addr, stuff_in, stuff_out, $time);
       #10 write_enable = 1;
-      #10 stuff_in = 8'b11111111;
-      #11$display("Test: %d", stuff_out);
-      #16 $display("Test: %d", stuff_out);
-   end
-    
-   always 
-    #2 clk = !clk;
+       stuff_in = 8'b1111111;
+      #15 write_enable= 0;
 
-   initial 
-      #100  $finish; 
-  
+      #20 addr = 8'b0000_0000;
+
+   end
+
+   always
+     #1 clk = !clk;
+
+   initial
+     #100  $finish;
+
 endmodule
