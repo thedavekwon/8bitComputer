0309e8f9e932533892c0ed18d440a41e5e8be682
thedavekwon <thedavekwon@gmail.com>
2016-04-18 20:16:24 -0400

    updated

--- controlunit.v
+++ controlunit.v
@@ -1,7 +1,23 @@
-module controlunit(instruction, dout1, dout2, dout3);
+module controlunit(clk, instruction, con_inst, con_reg);
    
-   input instruction;
-   output [7:0] dout1. dout2, dout3;
+   input [7:0] instruction;
+   input clk;
+   output [1:0] con_inst;
+   output [5:0] con_reg;
    
+   wire [7:0] instruction;
+   wire clk;
+   reg [1:0] con_inst;
+   reg [2:0] trunc_8_to_3;
+   reg [5:0] con_reg; trunc_8_to_5;
+
+   trunc_8_to_5 = instruction[5:0];
+   trunc_8_to_3 = instruction[7:5];
+   
+   always @ (clk)
+   begin
+   	con_reg = trunc_8_to_5;
+   	if(trunc_8_to_3 == 1)
+   end
   
 endmodule
