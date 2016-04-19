620ff6cd9cb920873a81e514e0d3a8a7e3ea379d
thedavekwon <thedavekwon@gmail.com>
2016-04-19 15:46:27 -0400

    tested

--- pc.v
+++ pc.v
@@ -1,7 +1,40 @@
-module pc(din, dout);
+module pc(din, dout, clk, control);
    
-   input din;
-   output dout;
-   
-  
+   input [7:0] din;
+   input clk, control;
+   output reg [7:0] dout;
+
+   always @ (posedge clk)begin
+   	if(control == 1)begin
+   		dout = din;
+   	end
+   	else begin 
+   		dout = dout + 1;
+   	end 
+   end
+ endmodule
+
+module test;
+   reg [7:0] stuff_in;
+   wire [7:0] stuff_out;
+   reg        clk, control;
+
+   pc pcounter(stuff_in, stuff_out, clk, control);
+
+   initial begin
+   	  #1 clk = 0;
+   	  control = 1;
+   	  stuff_in = 8'b1010_1010;
+   	  $monitor("%b, %d, %b, %b: time: %d",clk, control, stuff_in, stuff_out, $time);
+      #40 control = 0;
+
+
+   end
+
+   always
+     #1 clk = !clk;
+
+   initial
+     #100  $finish;
+
 endmodule
