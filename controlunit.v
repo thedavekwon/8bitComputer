4455f2878f26012dfb62aaffa7f3397b8792ccc4
DanManN <dnahimov@gmail.com>
2016-04-19 22:58:16 -0400

    Control!

--- controlunit.v
+++ controlunit.v
@@ -1,54 +1,84 @@
-module controlunit(clk, instruction, con_inst, con_reg);
-   
-   input [7:0] instruction;
-   input clk;
-   output [1:0] con_inst;
-   output [4:0] con_reg;
-   
-   wire [7:0] instruction;
-   wire clk;
-   reg [1:0] con_inst;
-   reg [2:0] three_inst;
-   reg [4:0] con_reg, five_reg;
+module controlunit(
+                   input        clk,
+                   input [7:0]  instruction,
+                   output [1:0] cntr_alu,
+                   output       regWE,
+                   output       memWE,
+                   output       brnch,
+                   output       selAluIn,
+                   output       lw,
+                   output       accWE,
+                   output       selAccIn);
+   wire                         three_inst;
+   wire                         five_reg;
 
    always @ (instruction)
-   	{three_inst,five_reg} = instruction;
-   
-   always @ (clk)
-   begin
-   	con_reg = five_reg;
-   	if(three_inst == 3'b010) begin
-   		con_inst = 2'b00;
-   	end else if(three_inst == 3'b011) begin
-   		con_inst = 2'b01;
-   	end else if(three_inst == 3'b100) begin
-   		con_inst = 2'b10;
-   	end else if(three_inst == 3'b101) begin
-   		con_inst = 2'b11;
-   	end
+   	 {three_inst,five_reg} = instruction;
+
+   always begin
+      case (three_inst)
+        3'b000: begin
+           memWE = 0;
+           regWE = 0;
+           brnch = 0;
+           accWE = 1;
+           selAccIn = 0;
+        end
+        3'b001: begin
+           memWE = 0;
+           regWE = 0;
+           brnch = 0;
+           accWE = 1;
+           selAccIn = 1;
+        end
+        3'b010: begin
+           memWE = 0;
+           regWE = 1;
+           brnch = 0;
+           accWE = 0;
+           selAluIn = 1;
+           lw = 0;
+   		     cntr_alu = 2'b00;
+        end
+        3'b011: begin
+           memWE = 0;
+           regWE = 1;
+           brnch = 0;
+           accWE = 0;
+           selAluIn = 1;
+           lw = 0;
+           cntr_alu = 2'b01;
+        end
+        3'b100: begin
+           memWE = 0;
+           regWE = 0;
+           brnch = 1;
+           accWE = 0;
+           selAluIn = 0;
+   		     cntr_alu = 2'b10;
+        end
+        3'b101: begin
+           memWE = 0;
+           regWE = 1;
+           brnch = 0;
+           accWE = 0;
+           selAluIn = 1;
+           lw = 0;
+   		     cntr_alu = 2'b11;
+        end
+        3'b110: begin
+           memWE = 1;
+           regWE = 0;
+           brnch = 0;
+           accWE = 0;
+        end
+        3'b111: begin
+           memWE = 0;
+           regWE = 1;
+           brnch = 0;
+           accWE = 0;
+           lw = 1;
+        end
+   	  endcase
    end
-endmodule
-
-
-module test;
-   reg [7:0] stuff_in;
-   wire [4:0] reg_out;
-   wire [1:0] inst_out;
-   reg        clk, write_enable;
-
-   controlunit contr(clk, stuff_in, inst_out, reg_out);
-
-   initial begin
-   	  #1 clk = 0;
-   	  stuff_in = 8'b1101_0011;
-   	  $monitor("%b, %b, %b, %b: time: %d",clk, stuff_in, inst_out, reg_out, $time);
-      #50 stuff_in = 8'b01111111;
-   end
-
-   always
-     #1 clk = !clk;
-
-   initial
-     #100  $finish;
-
 endmodule
