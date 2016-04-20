8a6ebd329b3a681d22d0a526ffa2fbb340efc33f
brettstephen <sbrett118@yahoo.com>
2016-04-19 21:58:32 -0400

    implemented mux logic

--- mux.v
+++ mux.v
@@ -1,13 +1,33 @@
-module mux2to1(din1, din2, dout);
+module mux2to1(din1, din2, select, dout);
    
-   input din1, din2;
-   output dout;
+   input [7:0] din1, din2;
+   input select;
+   output [7:0] dout;
 
+   if(select == 1'b0) begin
+   	dout = din1;
+   	end
+   else if(select == 1'b1) begin
+   	dout = din2;
+   	end
 endmodule
 
-module mux4to1(din1, din2, din3, din4,  dout);
+module mux4to1(din1, din2, din3, din4, mCtrl, dout);
    
    input din1, din2, din3, din4;
+   input [1:0] mCtrl;
    output dout;
 
+   if(mCtrl == 2'b00) begin
+   	dout = din1;
+   	end
+   if(mCtrl == 2'b01) begin
+   	dout = din2;
+    end
+   if(mCtrl == 2'b10) begin
+   	dout = din3;
+    end
+   if(mCtrl == 2'b11) begin
+   	dout = din4;
+   end
 endmodule
