d1f6f7490e6e6246d02fd9212cf3e1c32cf82727
DanManN <dnahimov@gmail.com>
2016-04-19 22:01:18 -0400

    correct bit assignments

--- alu.v
+++ alu.v
@@ -1,33 +1,32 @@
-module alu (regInSAR, regIn, regOut, control, branch);
-   
-   input [7:0] regInSAR, regIn; //regInA = Set Aside Register, regIn = register from instruction
-   input [1:0] control;			//control = control from instruction 
-   output [7:0] regOut;			//regOut = output from alu
-   output branch;				//branch = 1 (branch), branch = 0 (not branch)
+module alu (
+            input [7:0]  dataInACC
+            input [7:0]  dataIn; //dataInA = Set Aside Register, dataIn = register from instruction
+            input [1:0]  control; //control = control from instruction
+            output [7:0] dataOut);	//dataOut = output from alu
 
-   reg [7:0] regOut;
-   reg branch;
-   wire [7:0] regInSAR, regIn;
-   wire [1:0] control;
- 
-   always @ (regInSAR or regIn)
+   reg [7:0]             dataOut;
+   reg [7:0]             branchAddr;
+   wire [7:0]            dataInACC, dataIn;
+   wire [1:0]            control;
 
-   if(control == 1'b00) begin            //add
-   		regOut = regInSAR + regIn;
-   end else if(control == 1'b01) begin   //nand
-    	regOut = regInSAR ~& regIn;
-   end else if(control == 1'b10) begin   //branch not zero
-   		if(regInSAR == 1'b0) begin
-   			branch = 1;
-   		end else begin
-   			branch = 0;
-   		end
-   end else if(control == 1'b11) begin   //set less than
-   		if(regInSAR < regIn) begin
-   			regOut = 1'b0000_0001;
-  	 	end else begin
-   			regOut = 1'b0000_0000;
-   		end
-   end
-   
-endmodule 
+   always @ (dataInACC or dataIn)
+
+     if(control == 2'b00) begin            //add
+   		  dataOut = dataInACC + dataIn;
+     end else if(control == 2'b01) begin   //nand
+    	  dataOut = dataInACC ~& dataIn;
+     end else if(control == 2'b10) begin   //branchAddr not zero
+   		  if(dataInACC == 0) begin
+   			   dataOut = dataIn[4:0];
+   		  end else begin
+   		     dataOut = 1;
+   		  end
+     end else if(control == 2'b11) begin   //set less than
+   		  if(dataInACC < dataIn) begin
+   			   dataOut = 8'b0000_0001;
+  	 	  end else begin
+   			   dataOut = 8'b0000_0000;
+   		  end
+     end
+
+endmodule
