d002f35167a62fe6255d5b6074f5c9227a56da7a
DanManN <dnahimov@gmail.com>
2016-04-20 21:22:47 -0400

    fixed sign

--- signextend.v
+++ signextend.v
@@ -1,30 +1,24 @@
 module signexten(
           output reg [7:0] dout,
-          input [4:0]      din,
-          input            control);
+          input [4:0]      din);
 
-   always @ (control) begin
-    dout <= {{3{din[2]}},din};	  
+   always @ (din) begin
+    dout = {{3{din[4]}},din};	  
    end
 endmodule
 
 module test;
    reg [4:0] stuff_in;
    wire [7:0] stuff_out;
-   reg        control;
 
-   signexten signextendedd(stuff_out, stuff_in, control);
+   signexten signextendedd(stuff_out, stuff_in);
 
    initial begin
-      #1 control = 0;
       stuff_in = 5'b10101;
-      $monitor("%b, %b, %b", control, stuff_in, stuff_out);
+      $monitor("%b, %b",stuff_in, stuff_out);
       #40 stuff_in =5'b00100;
   
    end
- 
-   always
-     #1 control = !control;
 
    initial
      #100  $finish;
