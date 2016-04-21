39e4de4b8ff049698458c04b9204272b838208a8
DanManN <dnahimov@gmail.com>
2016-04-20 21:27:35 -0400

    $

--- assembler.py
+++ assembler.py
@@ -64,7 +64,7 @@ class acmi(ALU3):
 	code = 1
 
 class move(ASM):
-	'''`move' instruction''' 
+	'''`move' instruction'''
 	def __init__(self, src, dest):
 		ASM.__init__(self)
 		self.src = src
@@ -114,33 +114,33 @@ def label(name):
 # Setup user environment
 # Add registers
 ENV["$zero"] = 0
-ENV["v0"] = 1
-ENV["v1"] = 2
-ENV["v2"] = 3
-ENV["v3"] = 4
-ENV["a0"] = 5
-ENV["a1"] = 6
-ENV["a2"] = 7
-ENV["a3"] = 8
-ENV["s0"] = 9
-ENV["s1"] = 10
-ENV["s2"] = 11
-ENV["s3"] = 12
-ENV["s4"] = 13
-ENV["s5"] = 14
-ENV["s6"] = 15
-ENV["s7"] = 16
-ENV["t0"] = 17
-ENV["t1"] = 18
-ENV["t2"] = 19
-ENV["t3"] = 20
-ENV["t4"] = 21
-ENV["t5"] = 22
-ENV["t6"] = 23
-ENV["t7"] = 24
-ENV["t8"] = 25
-ENV["t9"] = 26
-ENV["t10"] = 27
+ENV["$v0"] = 1
+ENV["$v1"] = 2
+ENV["$v2"] = 3
+ENV["$v3"] = 4
+ENV["$a0"] = 5
+ENV["$a1"] = 6
+ENV["$a2"] = 7
+ENV["$a3"] = 8
+ENV["$s0"] = 9
+ENV["$s1"] = 10
+ENV["$s2"] = 11
+ENV["$s3"] = 12
+ENV["$s4"] = 13
+ENV["$s5"] = 14
+ENV["$s6"] = 15
+ENV["$s7"] = 16
+ENV["$t0"] = 17
+ENV["$t1"] = 18
+ENV["$t2"] = 19
+ENV["$t3"] = 20
+ENV["$t4"] = 21
+ENV["$t5"] = 22
+ENV["$t6"] = 23
+ENV["$t7"] = 24
+ENV["$t8"] = 25
+ENV["$t9"] = 26
+ENV["$t10"] = 27
 ENV["$gp"]= 28
 ENV["$sp"] = 29
 ENV["$fp"] = 30
